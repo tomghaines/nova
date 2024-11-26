e9704108acfa7c691a51761e40ee2281b7f40dfc
georgeeburt <georgeeburt@icloud.com>
2024-11-26 18:40:45 +0000

    fix: ci pipeline - install dependency steps

--- eslint.config.js
+++ eslint.config.js
@@ -26,7 +26,12 @@ export default [
       'react/no-unescaped-entities': 'off',
       'react/prop-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-empty-object-type': 'off'
+      'react/no-unknown-property': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+      ]
     }
   }
 ];
