99efccd3c88119147c4a24f5b11d0c101331ada8
K <yukiyukaiyue@gmail.com>
2024-11-26 15:16:22 +0000

    feat: command bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import { AppSidebar } from '@/components/dashboard/app-sidebar';
+import { AppSidebar } from '@/components/dashboard/sidebar';
 import { Component as ChartComponent } from '@/components/dashboard/chart';
+import { CommandDemo } from '@/components/dashboard/command';
 import './globals.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -14,25 +15,31 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             {/* Main Content Area */}
             <div className='flex-1 relative'>
               {/* Sidebar Toggle Button */}
-              <div className='absolute top-0 left-0 p-2'>
+              <div className=' top-0 left-0 p-2'>
                 <SidebarTrigger />
               </div>
               {/* Main Content Area */}
-              <div className='flex items-center justify-center bg-white'>
+              <div className='flex-1 flex items-center justify-center'>
                 {/* Content Wrapper */}
-                <div className='w-[80%] max-w-3xl mt-20 text-center'>
+                <div className='w-[80%] max-w-3xl text-center px-6'>
+                  {/* Command Component */}
+                  <div className='mb-8'>
+                    <CommandDemo />
+                  </div>{' '}
                   {/* Title */}
                   <h1 className='text-4xl font-bold mb-4 text-gray-800'>
                     Sentiment Analysis
                   </h1>
                   {/* Description */}
                   <p className='mb-6 text-gray-600'>
-                    The use of natural language processing, text analysis,
+                    The use of natural language processing, textitt analysis,
                     computational linguistics based on X (formerly Twitter) to
                     systematically study the market behaviors.
                   </p>
                   {/* Chart Component */}
-                  <ChartComponent />
+                  <div className='flex justify-center'>
+                    <ChartComponent />
+                  </div>
                 </div>
               </div>
             </div>
