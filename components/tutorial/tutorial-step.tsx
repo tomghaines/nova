08407913ca5c8134e7943b6ee01c3309d9524538
george <georgeeburt@icloud.com>
2024-11-26 18:17:19 +0000

    fix: eslint fix and add .github directory with ci.yml workflow

--- components/tutorial/tutorial-step.tsx
+++ components/tutorial/tutorial-step.tsx
@@ -12,15 +12,19 @@ export function TutorialStep({
       <Checkbox
         id={title}
         name={title}
-        className={`peer absolute top-[3px] mr-2`}
+        className={'peer absolute top-[3px] mr-2'}
       />
       <label
         htmlFor={title}
-        className={`relative text-base font-medium text-foreground peer-checked:line-through`}
+        className={
+          'relative text-base font-medium text-foreground peer-checked:line-through'
+        }
       >
         <span className='ml-8'>{title}</span>
         <div
-          className={`ml-8 text-sm font-normal text-muted-foreground peer-checked:line-through`}
+          className={
+            'ml-8 text-sm font-normal text-muted-foreground peer-checked:line-through'
+          }
         >
           {children}
         </div>
