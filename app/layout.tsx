1e7c0eed7151074220b8fb9b4b3fc8ff9b0ff62f
K <yukiyukaiyue@gmail.com>
2024-11-27 12:35:08 +0000

    feat: add spinner

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import './globals.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  
+
   const handleAuthentication = () => {
     setIsAuthenticated(true); // Update state when Auth signals success
   };
@@ -26,51 +26,50 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <SidebarProvider>
             <div className='flex min-h-screen'>
               {/* Sidebar */}
-              <AppSidebar className='flex-none w-64' />
-              
+              <AppSidebar className='w-64 flex-none' />
               {/* Main Content Area */}
-              <div className='flex-1 relative w-full mt-10 mb-2 ml-2 mr-2'>
+              <div className='relative mb-2 ml-2 mr-2 mt-10 w-full flex-1'>
                 {/* dark mode switch */}
-                <div className='fixed top-12 right-6'>
+                <div className='fixed right-6 top-12'>
                   <SwitchDemo />
                 </div>
-                
+
                 {/* Sidebar Trigger */}
-                <div className='absolute top-0 left-0 p-2'>
+                <div className='absolute left-0 top-0 p-2'>
                   <SidebarTrigger />
                 </div>
-                
+
                 {/* Main Content Wrapper */}
-                <div className='flex flex-col items-center justify-start w-full '>
+                <div className='flex w-full flex-col items-center justify-start'>
                   {/* Inner Container for Charts and Search */}
-                  <div className='w-[90%] max-w-4xl text-center px-6 pl-[100px]'>
+                  <div className='w-[90%] max-w-4xl px-6 pl-[100px] text-center'>
                     {/* Search Bar */}
                     <div className='mb-8'>
                       <CommandDemo />
                     </div>
-                    
+
                     {/* Sentiment Chart Section */}
                     <div className='mb-16'>
-                      <h1 className='text-4xl font-bold mb-4 text-gray-800 dark:text-gray-100'>
+                      <h1 className='mb-4 text-4xl font-bold text-gray-800 dark:text-gray-100'>
                         Sentiment Analysis
                       </h1>
                       <p className='mb-6 text-gray-600 dark:text-gray-400'>
                         The use of natural language processing, text analysis,
-                        computational linguistics based on X (formerly Twitter) to
-                        systematically study the market behaviors.
+                        computational linguistics based on X (formerly Twitter)
+                        to systematically study the market behaviors.
                       </p>
                       <SentimentChart />
                     </div>
-                    
+
                     {/* Mindshare Map Section */}
                     <div>
-                      <h1 className='text-4xl font-bold mb-4 text-gray-800 dark:text-gray-100'>
+                      <h1 className='mb-4 text-4xl font-bold text-gray-800 dark:text-gray-100'>
                         Mindshare Map
                       </h1>
                       <p className='mb-6 text-gray-600 dark:text-gray-400'>
                         A systematic representation of how the collective
-                        attention on X shifts across critical concepts, aiding in
-                        understanding market trends and focus distribution.
+                        attention on X shifts across critical concepts, aiding
+                        in understanding market trends and focus distribution.
                       </p>
                       <MindshareMap />
                     </div>
@@ -82,5 +81,5 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         )}
       </body>
     </html>
-  );  
+  );
 }
