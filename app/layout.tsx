e5b2b4fd372a9da50cbcd93e42e79746e6eae4aa
K <yukiyukaiyue@gmail.com>
2024-11-27 21:26:27 +0000

    bugfix: trigger icon size and position

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,26 +17,26 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body>
-          <SidebarProvider>
-            <div className='flex min-h-screen'>
-              {/* Sidebar */}
-              <AppSidebar />
-              {/* Main Content Area */}
-              <div className='relative mb-2 ml-2 mr-2 mt-10 w-full flex-1'>
-                {/* dark mode switch */}
-                <div className='fixed right-12 top-12'>
-                  <SwitchDemo />
-                </div>
-
-                {/* Sidebar Trigger */}
-                <div className='absolute left-0 top-0 p-2'>
-                  <SidebarTrigger />
-                </div>
-
-                <div>{children}</div>
+        <SidebarProvider>
+          <div className='flex min-h-screen'>
+            {/* Sidebar */}
+            <AppSidebar />
+            {/* Main Content Area */}
+            <div className='relative mb-2 ml-2 mr-2 mt-10 w-full flex-1'>
+              {/* dark mode switch */}
+              <div className='fixed right-12 top-12'>
+                <SwitchDemo />
               </div>
+
+              {/* Sidebar Trigger */}
+              <div className='absolute left-4 top-0 p-0'>
+                <SidebarTrigger />
+              </div>
+
+              <div>{children}</div>
             </div>
-          </SidebarProvider>
+          </div>
+        </SidebarProvider>
       </body>
     </html>
   );
