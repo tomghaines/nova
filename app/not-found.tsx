d1dcb50e9e99b48bc1bcaa3676f0571acaf16ea5
georgeeburt <georgeeburt@icloud.com>
2024-11-28 18:43:39 +0000

    chore: remove default favicon.ico

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <h1 className='mb-4 text-4xl font-bold'>404</h1>
       <h2 className='mb-8 text-xl'>Page Not Found</h2>
       <p className='mb-8 text-gray-600 dark:text-gray-400'>
-        The page you're looking for doesn't exist or has been moved.
+        The page you're looking for doesn&apos;t exist or has been moved.
       </p>
       <Link
         href='/home'
