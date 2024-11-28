6631d28211fa52773dcbcebd5072eeb4e0ec2c09
Karsten <karstenba@protonmail.com>
2024-11-28 11:56:12 +0100

    fix: token dropdown behavior

--- components/dashboard/command.tsx
+++ components/dashboard/command.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 
 import {
   Command,
@@ -12,37 +12,46 @@ import { Button } from '@/components/ui/button';
 
 export function SearchBar() {
   const [isTokenOpen, setIsTokenOpen] = useState(false);
-  // const [selectedToken, setSelectedToken] = useState('$BTC');
+  const [selectedMenuItem, setSelectedMenuItem] = useState('$BTC');
+  const [selectedToken, setSelectedToken] = useState('$BTC');
+
+  const onSelectToken = (e: MouseEvent) => {
+    e.preventDefault();
+    setSelectedToken((e.target as HTMLElement).innerText);
+    setIsTokenOpen(false)
+  };
 
   return (
     <div className='flex w-full items-center gap-5'>
       <div className='w-1/4'>
         <Command
           className='h-auto rounded-lg border shadow-md dark:shadow-lg dark:shadow-gray-500'
-          onBlur={() => setIsTokenOpen(false)}
+            value={selectedMenuItem}
+            onValueChange={setSelectedMenuItem}
+            onMouseEnter={() => setIsTokenOpen(true)}
+            onMouseLeave={() => setIsTokenOpen(false)}
         >
           <CommandInput
-            // value={selectedToken}
-            onFocus={() => setIsTokenOpen(true)}
+            placeholder={selectedToken}
             className='w-full cursor-pointer'
           />
           {isTokenOpen && (
-            <CommandList className='absolute top-full z-50 w-1/5 bg-white shadow-lg dark:bg-gray-800'>
-              <CommandItem>
+            <CommandList onClick={onSelectToken} className='absolute top-full z-50 w-1/5 bg-white shadow-lg dark:bg-gray-800'>
+              <CommandItem value="$BTC">
                 <img
                   src='https://cryptologos.cc/logos/bitcoin-btc-logo.png'
                   className='h-4 w-4'
                 />
                 <span className='text-base font-semibold'>$BTC</span>
               </CommandItem>
-              <CommandItem>
+              <CommandItem value="$ETH">
                 <img
                   src='https://cdn.worldvectorlogo.com/logos/ethereum-eth.svg'
                   className='h-4 w-4'
                 />
                 <span className='text-base font-semibold'>$ETH</span>
               </CommandItem>
-              <CommandItem>
+              <CommandItem value="$SOL">
                 <img
                   src='https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png'
                   className='h-4 w-4'
