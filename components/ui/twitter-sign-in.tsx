9680fa9283d253fce803b4873098dc05fb507f68
Mel <melzhou1204@gmail.com>
2024-11-28 10:27:19 +0000

    fix twitter sign-in button

--- components/ui/twitter-sign-in.tsx
+++ components/ui/twitter-sign-in.tsx
@@ -2,16 +2,18 @@
 
 import { signInWithTwitterAction } from '@/app/actions';
 import { FaTwitter } from 'react-icons/fa';
+import { Button } from '@/components/ui/button';
 
 export function TwitterSignIn() {
   return (
-    <button
+    <Button
       onClick={() => signInWithTwitterAction()}
       type='button'
-      className='flex w-full items-center justify-center gap-2 rounded-md bg-gray-900 px-4 py-2 text-white hover:bg-gray-700'
+      className='w-full'
+      variant='default'
     >
       <FaTwitter />
       Sign In with Twitter
-    </button>
+    </Button>
   );
 }
