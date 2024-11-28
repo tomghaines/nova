4be75bc690e6abaac18fdfcf6da486b38b55b76a
K <yukiyukaiyue@gmail.com>
2024-11-28 08:39:47 +0000

    feat: token menu 2.0 with logo

--- components/dashboard/command.tsx
+++ components/dashboard/command.tsx
@@ -10,11 +10,10 @@ import {
 
 export function CommandDemo() {
   const [isTokenOpen, setIsTokenOpen] = useState(false);
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   return (
     <div className='flex w-full gap-5'>
-      <div className='w-1/5'>
+      <div className='w-1/4'>
         <Command
           className='h-auto rounded-lg border shadow-md dark:shadow-lg dark:shadow-gray-500'
           onBlur={() => setIsTokenOpen(false)}
@@ -26,27 +25,35 @@ export function CommandDemo() {
           {isTokenOpen && (
             <CommandList>
               <CommandItem>
-                <span>$BTC</span>
+                <img
+                  src='https://cryptologos.cc/logos/bitcoin-btc-logo.png'
+                  className='h-4 w-4'
+                />
+                <span className='text-base font-semibold'>$BTC</span>
               </CommandItem>
               <CommandItem>
-                <span>$ETH</span>
+                <img
+                  src='https://cdn.worldvectorlogo.com/logos/ethereum-eth.svg'
+                  className='h-4 w-4'
+                />
+                <span className='text-base font-semibold'>$ETH</span>
               </CommandItem>
               <CommandItem>
-                <span>$SOL</span>
+                <img
+                  src='https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png'
+                  className='h-4 w-4'
+                />
+                <span className='text-base font-semibold'>$SOL</span>
               </CommandItem>
             </CommandList>
           )}
         </Command>
       </div>
 
-      <div className='w-4/5'>
-        <Command
-          className='h-auto rounded-lg border shadow-md dark:shadow-lg dark:shadow-gray-500'
-          onBlur={() => setIsSearchOpen(false)}
-        >
+      <div className='w-3/4'>
+        <Command className='h-auto rounded-lg border shadow-md dark:shadow-lg dark:shadow-gray-500'>
           <CommandInput
             placeholder='Type keywords or topics...'
-            onFocus={() => setIsSearchOpen(true)}
             className='w-full'
           />
         </Command>
--- components/dashboard/switch.tsx
+++ components/dashboard/switch.tsx
@@ -29,7 +29,7 @@ export function SwitchDemo({
   return (
     <div className='flex items-center space-x-2'>
       {/* Dynamic text based on state */}
-      <p className='text-gray-800 dark:text-gray-100'>
+      <p className='p-2 text-gray-800 dark:text-gray-100'>
         {isNightMode ? 'Night Mode' : 'Day Mode'}
       </p>
       {/* Switch component with state toggle */}
