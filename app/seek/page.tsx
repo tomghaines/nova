512c08b9682c25c13dcd980a5c91938aa24671c8
K <yukiyukaiyue@gmail.com>
2024-11-29 12:14:29 +0000

    feat: sentiment layout + modurize files

--- app/seek/page.tsx
+++ app/seek/page.tsx
@@ -1,20 +1,18 @@
 'use client';
 
-import { SentimentChart } from '@/components/dashboard/sentimentChart';
 import { MindshareMap } from '@/components/dashboard/mindshareMap';
 import { SearchBar } from '@/components/dashboard/command';
 import { SkeletonBar } from '@/components/dashboard/skeleton';
-import { ChartLine } from 'lucide-react';
-import React, { useState, useEffect } from 'react';
+import SentimentLayout from '@/components/seek/sentimentLayout';
+import React, { useState } from 'react';
 
 export default function page() {
-  const [isSentimentChartLoading, setIsSentimentChartLoading] = useState(true);
   const [isMindshareMapLoading, setIsMindshareMapLoading] = useState(true);
 
   return (
     <div className='flex w-full flex-col items-center justify-start'>
       {/* Inner Container for Charts and Search */}
-      <div className='w-[100%] max-w-4xl px-6 text-center'>
+      <div className='w-full max-w-4xl px-6 text-center'>
         {/* Search Bar */}
         <div className='relative mb-16 ml-10'>
           <SearchBar />
@@ -23,28 +21,8 @@ export default function page() {
         {/* AI-Summary */}
         <div className='AI-Summary'></div>
 
-        {/* Sentiment Chart Section - Outer */}
-        <div className='border bg-zinc-50'>
-          {/* Sentiment Chart Section - Heading */}
-          <div className='flex h-8 w-full border-zinc-300 bg-zinc-300 pl-4 pt-1 text-left font-sans font-medium text-gray-500'>
-            <ChartLine className='mr-2 mt-1 h-4 w-4' /> Sentiment Analysis
-          </div>
-          {/* Sentiment Chart Section - Inner */}
-          <div className='mb-8 ml-6 mr-6 mt-8 flex flex-col items-center justify-start border'>
-            {/* <h1 className='mb-4 ml-20 text-4xl font-bold text-gray-800 dark:text-gray-100'>
-              Sentiment Analysis
-            </h1>
-            <p className='mb-6 ml-20 text-gray-600 dark:text-gray-400'>
-              The use of natural language processing, text analysis, computational
-              linguistics based on X (formerly Twitter) to systematically study
-              the market behaviors.
-            </p> */}
-            {isSentimentChartLoading && <SkeletonBar />}
-            <SentimentChart
-              onLoadComplete={() => setIsSentimentChartLoading(false)}
-            />
-          </div>
-        </div>
+        {/* Sentiment Layout Section */}
+        <SentimentLayout />
 
         {/* Mindshare Map Section */}
         <div className='flex flex-col items-center justify-center'>
---
+++ components/seek/sentimentLayout.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { SentimentChart } from '@/components/dashboard/sentimentChart';
+import { SkeletonBar } from '@/components/dashboard/skeleton';
+import { ChartLine, TrendingUp, TrendingDown } from 'lucide-react';
+import React, { useState } from 'react';
+
+export default function SentimentLayout() {
+  const [isSentimentChartLoading, setIsSentimentChartLoading] = useState(true);
+
+  return (
+    <div className='border'>
+      {/* Sentiment Chart Section - Heading */}
+      <div className='flex h-8 w-full border-zinc-300 bg-zinc-300 pl-4 pt-1 text-left font-sans font-medium text-gray-500'>
+        <ChartLine className='mr-2 mt-1 h-4 w-4' /> Sentiment Analysis
+      </div>
+      {/* Sentiment Chart Section - Inner */}
+      <div className='mb-8 ml-6 mr-6 mt-8 flex flex-col items-center justify-start border'>
+        {isSentimentChartLoading && <SkeletonBar />}
+        <SentimentChart
+          onLoadComplete={() => setIsSentimentChartLoading(false)}
+        />
+      </div>
+
+      {/* Sentiment Chart Section - Opinions */}
+      <div className='flex justify-between gap-x-6'>
+        {/* Sentiment Chart Section - Opinions Bullish */}
+        <div className='mb-6 ml-6 h-40 w-1/2 border-2'>
+          <div className='flex h-8 w-full border-zinc-300 bg-zinc-300 pl-4 pt-1 text-left font-sans font-medium text-gray-500'>
+            <TrendingUp className='mr-2 mt-1 h-4 w-4' /> Bullish
+          </div>
+        </div>
+        {/* Sentiment Chart Section - Opinions Bearish */}
+        <div className='mb-6 mr-6 h-40 w-1/2 border-2'>
+          <div className='flex h-8 w-full border-zinc-300 bg-zinc-300 pl-4 pt-1 text-left font-sans font-medium text-gray-500'>
+            <TrendingDown className='mr-2 mt-1 h-4 w-4' /> Bearish
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
