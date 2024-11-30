d10aa0aac7aa2195522e9c7d90a8c7b250f76df3
K <yukiyukaiyue@gmail.com>
2024-11-30 09:51:15 +0000

    feat: styling mindshare

--- app/(protected)/seek/page.tsx
+++ app/(protected)/seek/page.tsx
@@ -4,11 +4,10 @@ import { MindshareMap } from '@/components/features/dashboard/mindshareMap';
 import { SearchBar } from '@/components/features/dashboard/command';
 import { SkeletonBar } from '@/components/features/dashboard/skeleton';
 import SentimentLayout from '@/components/features/seek/sentimentLayout';
-import React, { useState } from 'react';
+import MindshareLayout from '@/components/features/seek/mindshareLayout';
+import React from 'react';
 
 export default function page() {
-  const [isMindshareMapLoading, setIsMindshareMapLoading] = useState(true);
-
   return (
     <div className='flex w-full flex-col items-center justify-start'>
       {/* Inner Container for Charts and Search */}
@@ -25,20 +24,7 @@ export default function page() {
         <SentimentLayout />
 
         {/* Mindshare Map Section */}
-        <div className='flex flex-col items-center justify-center'>
-          <h1 className='mb-4 ml-20 mt-10 text-4xl font-bold text-gray-800 dark:text-gray-100'>
-            Mindshare Map
-          </h1>
-          <p className='mb-6 ml-20 text-gray-600 dark:text-gray-400'>
-            A systematic representation of how the collective attention on X
-            shifts across critical concepts, aiding in understanding market
-            trends and focus distribution.
-          </p>
-          {isMindshareMapLoading && <SkeletonBar />}
-          <MindshareMap
-            onLoadComplete={() => setIsMindshareMapLoading(false)}
-          />
-        </div>
+        <MindshareLayout />
       </div>
     </div>
   );
