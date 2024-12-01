785a9d23e72b6e96d71a2c30eae6f06ef4d0c44a
K <yukiyukaiyue@gmail.com>
2024-12-01 21:13:21 +0000

    feat: default dark mode

--- components/features/dashboard/sidebar.tsx
+++ components/features/dashboard/sidebar.tsx
@@ -77,64 +77,73 @@ export function AppSidebar({ isNightMode }: { isNightMode: boolean }) {
 
   return (
     <Sidebar>
-      <SidebarContent className=''>
-        <SidebarGroup>
-          <SidebarGroupContent className='mt-24 w-full'>
-            <SidebarMenu className='space-y-6'>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link href={item.url}>
-                      <span className='h-18 dark:border-invert mt-6 w-full border-t-2 border-gray-600 p-2 text-xl font-thin hover:font-black'>
-                        {item.title}
-                      </span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      {user && (
-        <SidebarFooter>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <SidebarMenuButton className='flex justify-around gap-1'>
-                <Avatar className='h-6 w-6 overflow-hidden rounded-full'>
-                  <AvatarImage
-                    src={
-                      user.user_metadata.avatar_url
-                        ? user.user_metadata.avatar_url
-                        : 'https://github.com/shadcn.png'
-                    }
-                    alt='@shadcn'
-                  />
-                </Avatar>
-                <span className='flex-1'>{user.email}</span>
-                <ChevronUp className='ml-2' />
-              </SidebarMenuButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent
-              side='top'
-              className='w-[--radix-popper-anchor-width]'
-            >
-              <DropdownMenuItem>
-                <Button className='w-full text-left'>
-                  <a href='/account'>Account</a>
-                </Button>
-              </DropdownMenuItem>
-              <form action={signOutAction} onSubmit={handleSignOut}>
-                <DropdownMenuItem asChild>
-                  <Button type='submit' className='w-full hover:cursor-pointer'>
-                    <a href='/home'>Sign out</a>
+      <SidebarContent className='flex h-full flex-col'>
+        <div className='mt-auto'>
+          <SidebarGroup>
+            <SidebarGroupContent className='w-full'>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      asChild
+                      className='focus:outline-none active:bg-transparent'
+                    >
+                      <Link href={item.url}>
+                        <span className='h-18 dark:border-invert w-full p-2 text-2xl font-thin hover:font-black'>
+                          {item.title}
+                        </span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </div>
+
+        {user && (
+          <SidebarFooter className='mt-2'>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton className='flex justify-around gap-1'>
+                  <Avatar className='h-6 w-6 overflow-hidden rounded-full'>
+                    <AvatarImage
+                      src={
+                        user.user_metadata.avatar_url
+                          ? user.user_metadata.avatar_url
+                          : 'https://github.com/shadcn.png'
+                      }
+                      alt='@shadcn'
+                    />
+                  </Avatar>
+                  <span className='flex-1'>{user.email}</span>
+                  <ChevronUp className='ml-2' />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side='top'
+                className='w-[--radix-popper-anchor-width]'
+              >
+                <DropdownMenuItem>
+                  <Button className='w-full text-left'>
+                    <a href='/account'>Account</a>
                   </Button>
                 </DropdownMenuItem>
-              </form>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </SidebarFooter>
-      )}
+                <form action={signOutAction} onSubmit={handleSignOut}>
+                  <DropdownMenuItem asChild>
+                    <Button
+                      type='submit'
+                      className='w-full hover:cursor-pointer'
+                    >
+                      <a href='/home'>Sign out</a>
+                    </Button>
+                  </DropdownMenuItem>
+                </form>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarFooter>
+        )}
+      </SidebarContent>
     </Sidebar>
   );
 }
